22a107afb81b2082b461ab60ddd6cbc9e9a3c716
Alex Malykh <malykhs1955@yandex.ru>
2021-06-19 16:14:20 +0300

    fix

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,13 @@
 
 const showInputError = (formElement, inputElement, errorMessage, config ) => {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
     inputElement.classList.add(config.inputErrorClass);
     errorElement.classList.add(config.errorClass); 
     errorElement.textContent = errorMessage;
 }
 
 const hideInputError = (formElement, inputElement, config) => {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
     inputElement.classList.remove(config.inputErrorClass);
     errorElement.classList.remove(config.errorClass); 
     errorElement.textContent = '';
